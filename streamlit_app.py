f084686a0c4006eaa68594eee331783d2f9c5e0d
Mahapan Leyser <104196346+mleyser@users.noreply.github.com>
2022-05-17 00:31:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,16 @@ my_cur.execute("SELECT CURRENT_USER, CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 st.text(my_data_row)
 
+st.markdown("---")
+
 
 # actual team info
 st.title('SC Magdeburg | Saison 2021/22')
 
 image = Image.open('kader_magdeburg.jpg')
 st.image(image)
+
+st.markdown("---")
 
 st.header("Teamstatistik")
 
@@ -33,23 +37,47 @@ col1.markdown("**Gegentore**: 751")
 col1.markdown("**Differenz**: 142")
 col1.markdown("**Bester 7m-Schütze**: Omar Ingi Magnusson(90/121/91.5%)")
 col1.markdown("**Sünder**: Piotr Chrapkowski(51)")
-col1.markdown("**Ticketkäufe**: 107477")
+col1.markdown("**Ticketverkäufe**: 107477")
 
 
 tabellenstand = Image.open('tabelle_hist_magdeburg.png')
 col2.image(tabellenstand)
 
-
+st.markdown("---")
 
 st.header("Spieler Gesamtstatistik")
 
 
-st.text("Spaltennamen | 0️⃣: Nachname, 1️⃣: Vorname, 2️⃣: POS, 3️⃣: S, 4️⃣: T, 5️⃣: FW, 6️⃣: FT, 7️⃣: 7M")
-st.text("8️⃣: %, 9️⃣: AS, 1️⃣0️⃣: TF, 1️⃣1️⃣: ST, 1️⃣2️⃣: BL, 1️⃣3️⃣: GK, 1️⃣4️⃣: 2M, 1️⃣5️⃣: RK, 1️⃣6️⃣: BK")
-my_cur.execute("SELECT * FROM kader_magdeburg")
-my_data_rows = my_cur.fetchall()
-data_berlin = st.dataframe(my_data_rows)
+#st.text("Spaltennamen | 0️⃣: Nachname, 1️⃣: Vorname, 2️⃣: POS, 3️⃣: S, 4️⃣: T, 5️⃣: FW, 6️⃣: FT, 7️⃣: 7M")
+#st.text("8️⃣: %, 9️⃣: AS, 1️⃣0️⃣: TF, 1️⃣1️⃣: ST, 1️⃣2️⃣: BL, 1️⃣3️⃣: GK, 1️⃣4️⃣: 2M, 1️⃣5️⃣: RK, 1️⃣6️⃣: BK")
+#my_cur.execute("SELECT * FROM kader_magdeburg")
+#my_data_rows = my_cur.fetchall()
+#data_berlin = st.dataframe(my_data_rows)
+
+# Spielwiese
+
+@st.cache
+def get_data_from_csv():
+  df = pd.read_csv("kader_magdeburg_index_false.csv")
+  return df
+df = get_data_from_csv()
+  
+rad = st.sidebar.radio("Navigation",["Gesamtansicht","Gefilterte Ansicht"])
 
 # Sidebar
 
-st.sidebar.header("Hier Filter wählen:")
+if rad == "Gesamtansicht":
+  st.text("Erstansicht absteigend nach geworfenen Toren sortiert")
+  st.dataframe(df)
+
+if rad == "Gefilterte Ansicht":
+  st.text("Erstansicht zeigt Stammtorhüter")
+  st.sidebar.header("Hier Filter wählen:")
+
+  positionen = st.sidebar.multiselect("Spielposition (Angriffsmodus):", options = df["Position"].unique(), default = df["Position"].unique())
+
+  nachname = st.sidebar.multiselect("Spieler (Nachname):",options = df["Nachname"].unique(), default = ["Green"])
+
+  df_selection = df.query("Position == @positionen & Nachname == @nachname")
+
+  st.dataframe(df_selection)
